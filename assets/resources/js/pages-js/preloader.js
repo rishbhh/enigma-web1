58d1a069deecdf44d420703414465b8588a60862
rishbhh <40095968+rishbhh@users.noreply.github.com>
2022-04-21 20:44:13 +0530

    updatee

--- assets/resources/js/pages-js/preloader.js
+++ assets/resources/js/pages-js/preloader.js
@@ -1,281 +1,136 @@
-if (!isMobile) {
+(function($) {
+    "use strict";
+    console.clear();
+    gsap.registerPlugin( ScrollTrigger,);
+    var pageSet, pageCursor, siteLoader, headerStick, smoothScroll, siteHeader, pageLayout, headerLayout, footerLayout, menuLayout, menuStyle;
 
-    let preloader = function () {
-        let loadOverlay = document.querySelector(".init-load")
-        let overlayInner = document.querySelector(".init-load .inner")
-        let count1 = document.querySelector(".c-first .count")
-        let count2 = document.querySelector(".c-second .count")
-        let count3 = document.querySelector(".c-third .count")
-        let percentage = document.querySelector(".init-load .percentage")
-        let countHeight1 = document.querySelector(".counter.c-first .count").offsetHeight
-        let countHeight2 = document.querySelector(".counter.c-second .count").offsetHeight
-        let countHeight3 = document.querySelector(".counter.c-third .count").offsetHeight
-        let counterFrag1 = countHeight1 / 2
-        let counterFrag2 = countHeight2 / 11
-        let counterFrag3 = countHeight3 / 101
-        console.log(countHeight3)
-
-        gsap.set(count3, { y: counterFrag3 })
-        gsap.set(count2, { y: counterFrag2 })
-        gsap.set(count1, { y: counterFrag1 })
-        gsap.set(loadOverlay, { opacity: 1 })
-        gsap.set(percentage, { opacity: 0 })
-
-
-        let countTimeline = gsap.timeline()
-        countTimeline.to(count3, {
-            y: -counterFrag1 * 25,
-            duration: 1,
-            ease: "power4.inOut"
-        })
-        countTimeline.to(count2, {
-            y: -counterFrag1 * 2,
-            duration: 1,
-            ease: "power4.inOut"
-        }, "-=1")
-        countTimeline.to(count1, {
-            y: 0,
-            duration: 1,
-            ease: "power4.inOut"
-        }, "-=1")
-        countTimeline.to(percentage, {
-            opacity: 1,
-            duration: 1,
-            ease: "power4.inOut"
-        }, "-=1")
-        countTimeline.to(count3, {
-            y: -counterFrag1 * 48,
-            duration: .5,
-            ease: "power4.inOut"
-        })
-        countTimeline.to(count2, {
-            y: -counterFrag1 * 4,
-            duration: .5,
-            ease: "power4.inOut"
-        }, "-=.5")
-        countTimeline.to(count3, {
-            y: -counterFrag1 * 74,
-            duration: .8,
-            ease: "power4.inOut"
-        })
-        countTimeline.to(count2, {
-            y: -counterFrag1 * 7,
-            duration: .8,
-            ease: "power4.inOut"
-        }, "-=.8")
-        countTimeline.to(count3, {
-            y: -counterFrag1 * 100,
-            duration: .65,
-            ease: "power4.inOut"
-        })
-        countTimeline.to(count2, {
-            y: -counterFrag1 * 10,
-            duration: .65,
-            ease: "power4.inOut",
-        }, "-=.65")
-        countTimeline.to(count1, {
-            y: -counterFrag1,
-            duration: .65,
-            ease: "power4.inOut",
-        }, "-=.65")
-        countTimeline.to(overlayInner, {
-            scale: 1,
-            duration: .3,
-            ease: "power4.inOut",
-            onComplete: intAnim
-        })
-        countTimeline.to(loadOverlay, {
-            x: "100%",
-            duration: 1,
-            ease: "power4.inOut"
-        }, "-=.4")
-
+    function pageSettings() {
+        pageSet = $('body');
+        pageCursor = pageSet.data('cursor');
+        siteLoader = pageSet.data('page-loader');
+        headerStick = pageSet.data('header-sticky');
+        smoothScroll = pageSet.data('smoothScroll');
+        menuStyle = pageSet.data('menu-style');
+        pageLayout = pageSet.data('page-layout');
+        headerLayout = pageSet.data('header-layout');
+        menuLayout = pageSet.data('menu-layout');
+        footerLayout = pageSet.data('footer-layout');
+        pageSet.addClass(pageLayout)
+        $('.site-footer').addClass(footerLayout);
     }
-    preloader()
+    pageSettings()
+    var loader, loaderOv, loadAn;
 
-
-
-} else {
-
-
-    let intAnim = function () {
-        // gsap.timeline({}).from("#b", {
-        //     y:100, duration: 2, ease: 'none', opacity: 0
-        // }, "-=1")
-        gsap.timeline({}).to(".scroll-circle", {
-            duration: 1, ease: 'power1.inOut', opacity:1, alpha:1,
-        },"1")
-        gsap.timeline({}).to(".scroll-circle .scroll-icon", {
-            rotation: 136,
-            duration: 1, ease: 'power1.inOut', opacity:1,
-        })
-        gsap.timeline({}).to(".scroll-arrow svg", {
-          y:-19,
-            duration: 1.1, ease: 'power1.in', opacity:1,
-        })
-
-    }
-
-
-    let preloader = function () {
-        let loadOverlay = document.querySelector(".init-load")
-        let overlayInner = document.querySelector(".init-load .inner")
-        let count1 = document.querySelector(".c-first .count")
-        let count2 = document.querySelector(".c-second .count")
-        let count3 = document.querySelector(".c-third .count")
-        let percentage = document.querySelector(".init-load .percentage")
-        let countHeight1 = document.querySelector(".counter.c-first .count").offsetHeight
-        let countHeight2 = document.querySelector(".counter.c-second .count").offsetHeight
-        let countHeight3 = document.querySelector(".counter.c-third .count").offsetHeight
-        let counterFrag1 = countHeight1 / 2
-        let counterFrag2 = countHeight2 / 11
-        let counterFrag3 = countHeight3 / 101
-        console.log(countHeight3)
-
-        gsap.set(count3, { y: counterFrag3 })
-        gsap.set(count2, { y: counterFrag2 })
-        gsap.set(count1, { y: counterFrag1 })
-        gsap.set(loadOverlay, { opacity: 1 })
-        gsap.set(percentage, { opacity: 0 })
-
-
-        let countTimeline = gsap.timeline()
-        countTimeline.to(count3, {
-            y: -counterFrag1 * 25,
-            duration: 1,
-            ease: "power4.inOut"
-        })
-        countTimeline.to(count2, {
-            y: -counterFrag1 * 2,
-            duration: 1,
-            ease: "power4.inOut"
-        }, "-=1")
-        countTimeline.to(count1, {
-            y: 0,
-            duration: 1,
-            ease: "power4.inOut"
-        }, "-=1")
-        countTimeline.to(percentage, {
-            opacity: 1,
-            duration: 1,
-            ease: "power4.inOut"
-        }, "-=1")
-        countTimeline.to(count3, {
-            y: -counterFrag1 * 48,
-            duration: .5,
-            ease: "power4.inOut"
-        })
-        countTimeline.to(count2, {
-            y: -counterFrag1 * 4,
-            duration: .5,
-            ease: "power4.inOut"
-        }, "-=.5")
-        countTimeline.to(count3, {
-            y: -counterFrag1 * 74,
-            duration: .8,
-            ease: "power4.inOut"
-        })
-        countTimeline.to(count2, {
-            y: -counterFrag1 * 7,
-            duration: .8,
-            ease: "power4.inOut"
-        }, "-=.8")
-        countTimeline.to(count3, {
-            y: -counterFrag1 * 100,
-            duration: .65,
-            ease: "power4.inOut"
-        })
-        countTimeline.to(count2, {
-            y: -counterFrag1 * 10,
-            duration: .65,
-            ease: "power4.inOut",
-        }, "-=.65")
-        countTimeline.to(count1, {
-            y: -counterFrag1,
-            duration: .65,
-            ease: "power4.inOut",
-        }, "-=.65")
-        countTimeline.to(".t", {
-            y:100,
-            duration: 1.4,
-            rotateX:"-80deg",
-            scaleX:0.9,
-            scaleY:0.9,
-            opacity:0,
-            stagger:0.1,
-            ease: "power4.inOut",
-        }, "-=.65")
+    function pageLoader() {
+        loader = $('.alioth-page-loader');
+        if (siteLoader == true) {
+            $('.apl-count').wrap('<div class="apl-wrapper"></div>');
+            var loaderLayout = loader.data('layout');
+            loader.addClass(loaderLayout)
+            const nums1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1],
+                nums2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+            let num1Text = '';
+            let num2Text = '';
+            for (let i = 0; i < nums1.length; i++) {
+                num1Text += '<span>' + nums1[i] + '</span>';
+            }
+            for (let i = 0; i < nums2.length; i++) {
+                num2Text += '<span>' + nums2[i] + '</span>';
+            }
+            $('.apl-count').append('<div class="apl-num apl-num-1"></div><div class="apl-num apl-num-2"></div><div class="apl-num apl-num-3"></div>')
+            $('.apl-num-1').html(num1Text);
+            $('.apl-num-2').html(num2Text);
+            $('.apl-num-3').html('<span>%</span><span>0</span>');
+            $('.apl-num').wrapInner('<div class="apl-num-wrapper"></div>')
+            var aplCount = loader.find('.apl-count'),
+                num1wrap = aplCount.find('.apl-num-1 .apl-num-wrapper'),
+                num2wrap = aplCount.find('.apl-num-2 .apl-num-wrapper'),
+                num3wrap = aplCount.find('.apl-num-3 .apl-num-wrapper'),
+                duration = loader.data('duration');
+            loadAn = gsap.timeline({
+                yoyo: true,
+                id: 'pageLoader',
+                once: true,
+                onStart: function() {
+                    $('body').addClass('loading');
+                },
+            });
+            loadAn.to(num1wrap, duration, {
+                y: '-91%',
+                ease: 'power2.inOut',
+            }, .25)
+            loadAn.to(num2wrap, duration, {
+                y: '-95.3%',
+                ease: 'power2.inOut',
+            }, .25)
+            loadAn.to(num3wrap, 1.5, {
+                y: '0%',
+                ease: 'power2.Out',
+            }, .5)
+            loadAn.to('.site-logo', 1, {
+                y: '0%',
+                ease: 'power2.out',
+            }, 2)
         
-        countTimeline.to(overlayInner, {   
-            scale: 1,
-            duration: .3,
-            ease: "power4.inOut",
-            onComplete: intAnim
-        })
-        countTimeline.to(loadOverlay, {
-            x: "100%",
-            duration: 1,
-            ease: "power4.inOut"
-        }, "-=.4")
-        countTimeline.to("#noise2", {
-            x: "100%",
-            duration: 1,
-            alpha:0,
-            ease: "power4.inOut"
-        }, "-=.3")
-        countTimeline.from(".el-bg",{opacity:0,
-            duration: 0.5,
-            ease: "power4.inOut"
-        }, "-=0.8")
-        countTimeline.from(".en-logo img",1.2,{opacity:0,
-            duration: 0.5,y:300,
-            ease: "power4.inOut"
-        }, "-=0.7")
-        countTimeline.from(".hro-img",1.2,{opacity:0,
-            duration: 0.5,y:300,
-            ease: "power4.inOut"
-        }, "-=0.7")
-        countTimeline.from(".module--header__bg",1,{opacity:0,
-            duration: 0.5,x:-100,
-            ease: "power4.inOut"
-        }, "-=0.6")
-        
-        countTimeline.from(".about-us-image img",1,{ y: "200",
-        duration: 1,
-        opacity:0,
-        transformOrigin:"bottom",
-        ease: "power4.inOut"
-    }, "-=1.2")
-    countTimeline.to(".tris_titles span",2,{ y: "0",
-        duration: 0.5,
-        ease: "power4.inOut"
-    }, "-=0.9")
-    countTimeline.from(".text-rveal span",1,{ y: "180",
-        duration:0.5,
-    }, "-=2")
-    countTimeline.from(".element",1,{ scale:0,
-        duration:0.5,opacity:0
-    }, "-=1.8")
-    countTimeline.from(".top-img img",1,{
-        duration:0.5,opacity:0,transformOrigin:"left",x:-100
-    }, "-=1.3")
-    countTimeline.from(".welcome-heading",2,{opacity:0,
-    duration: 1,
-    ease: "power4.inOut"
-}, "-=1.6")
-
-
-
+           
+        }
     }
-    preloader()
-
    
-}
 
-
-
-
-function isMobile() {
-return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-}
-// 
+ 
+    let mobileQuery = window.matchMedia('(max-width: 450px)');
+    if (!mobileQuery.matches) {
+       
+    }
+    $(window).on('load', function() {
+        if ($('.section.fullscreen').length) {
+            $('#footer').hide()
+        } else {
+            $('#footer').show()
+        }
+        pageLoader();
+        if (siteLoader == true) {
+            loadAn.eventCallback('onComplete', function() {
+                $('body').removeClass('loading');
+                gsap.set('#page', {
+                    visibility: 'visible'
+                })
+                gsap.to('.apl-background', .7, {
+                    height: '0%',
+                    ease: 'power2.inOut',
+                    onComplete: function() {
+                        loader.hide();
+                    }
+                })
+                showcaseOpenings();
+                initShortcodes();
+                initPages();
+                let mobileQuery = window.matchMedia('(max-width: 900px)');
+                if (!mobileQuery.matches) {
+                    aliothParallaxScroll();
+                }
+                enableScroll()
+                aliothScrollAnimations();
+                ScrollTrigger.refresh(true)
+            })
+        } else {
+            loader.hide();
+            if ($('.section.fullscreen').length) {
+                $('#footer').hide()
+            } else {
+                $('#footer').show()
+            }
+            showcaseOpenings();
+            initShortcodes();
+            initPages();
+            let mobileQuery = window.matchMedia('(max-width: 900px)');
+            if (!mobileQuery.matches) {
+                aliothParallaxScroll();
+            }
+            enableScroll()
+            aliothScrollAnimations();
+            ScrollTrigger.refresh(true)
+        }
+    })
+   
+}(jQuery));
