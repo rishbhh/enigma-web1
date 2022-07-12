fb31438277ca7c2fc80a1c2186fd59b39b58664d
rishbhh <sevikash04@gmail.com>
2022-07-12 20:05:59 +0530

    up

--- assets/resources/js/page.js
+++ assets/resources/js/page.js
@@ -1258,21 +1258,6 @@ $(document).on("click", ".form-close", function() {
     ClickForm.reversed(!ClickForm.reversed());
 });
 
-// Rangee
-// let rangeInput = document.querySelector(".range-input input");
-// let rangeValue = document.querySelector(".range-input .value div");
-
-// let  start = parseFloat(rangeInput.min);
-// let end = parseFloat(rangeInput.max);
-// let step = parseFloat(rangeInput.step);
-
-// for(let i=start;i<=end;i+=step){
-//   rangeValue.innerHTML += '<div>'+i+'</div>';
-// }
-// rangeInput.addEventListener("input",function(){
-//   let top = parseFloat(rangeInput.value)/step * -40;
-//   rangeValue.style.marginTop = top+"px";
-// });
 
 // Footer
 function Footer() {
